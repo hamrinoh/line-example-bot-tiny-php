04016df8015c9530c3c75b04a83a520c632cc41a
unknown <honggi7932@gmail.com>
2020-05-14 15:55:59 +0800

    revise map

--- includes/location.php
+++ includes/location.php
@@ -20,16 +20,17 @@
 }
 ==============================
 */
-if (strtolower($message['text']) == "location" || $message['text'] == "地址" || $message['text'] == "位置") {
+if (strtolower($message['text']) == "location" || $message['text'] == "地址" || $message['text'] == "位置" 
+    || $message['text'] == "wenzao" || $message['text'] == "文藻") {
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
         'messages' => array(
             array(
                 'type' => 'location', // 訊息類型 (位置)
                 'title' => 'Example location', // 回復標題
-                'address' => '台灣高雄市三民區大昌一路 98 號 (立志中學)', // 回復地址
-                'latitude' => 22.653742, // 地址緯度
-                'longitude' => 120.32652400000006 // 地址經度
+                'address' => '台灣高雄市三民區民族一路900號', // 回復地址
+                'latitude' => 22.67, // 地址緯度
+                'longitude' => 120.31906 // 地址經度
             )
         )
     ));
