529e7efc8ca62d890ca2fe09048b6b3cd1c9af14
hamrinoh <honggi7932@gmail.com>
2020-05-21 15:59:31 +0800

    invoice.php

--- includes/text.php
+++ includes/text.php
@@ -74,4 +74,58 @@ if ($message['text'] == "靜儀是天使嗎?"){
     ));
 }
 
+
+if ($message['text'] == "統一發票"){
+
+    $url = "http://invoice.etax.nat.gov.tw";
+
+    $page = file_get_contents($url);
+
+    $msg = "今天是".date("Y-m-d");
+
+    $msg = "";
+    if ($page) {
+
+        libxml_use_internal_errors(true);
+
+        $dom = new DOMDocument();
+        $dom->loadHTML($page);
+
+        // 用 tag 名稱擷取
+        $h2s = $dom->getElementsByTagName('h2');
+        echo "開獎期間：".$h2s[1]->textContent."\n";
+
+        $xpath = new DOMXPath($dom);
+        // 用 class 名稱擷取
+        $nodes = $xpath->query("//span[contains(@class,'t18Red')]");
+        $count = $nodes->length;
+        $dates = $xpath->query("//p[contains(@class,'date')]");
+        // echo "<p>Number:$count</p>";
+
+        if ($count>0) {
+            $msg .= "特別獎 ".$nodes[0]->textContent."\n";
+            $msg .= "特獎 ".$nodes[1]->textContent."\n";
+            $msg .= "頭獎 ".$nodes[2]->textContent."\n";
+            $msg .= "增開六獎 ".$nodes[3]->textContent."\n";
+            $msg .= $dates[0]->textContent;
+        } else {
+            echo "查無資料:";
+        }
+        libxml_clear_errors();
+    } else {
+        echo "無法取得網頁";
+    }
+
+
+    $client->replyMessage(array(
+        'replyToken' => $event['replyToken'],
+        'messages' => array(
+            array(
+                'type' => 'text', // 訊息類型 (文字)
+                'text' => $msg // 回復訊息
+            )
+        )
+    ));
+}
+
 ?>
